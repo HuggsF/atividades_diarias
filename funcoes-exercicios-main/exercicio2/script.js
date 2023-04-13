512c13dc0ea3e755d0ca5fa548ee395969e25ac8
Hugo Fernandes <104449782+HuggsF@users.noreply.github.com>
2023-04-13 00:18:20 -0300

    Adicionado exercícios de funções

---
+++ funcoes-exercicios-main/exercicio2/script.js
@@ -0,0 +1,15 @@
+soma = (n1, n2) => {return n1+n2}
+
+console.log(soma(50,150))
+
+maiorIgual = (n1,n2) => {return n1>=n2}
+
+console.log(maiorIgual(3,40))
+
+ehPar = (n1) => {return n1%2==0}
+
+console.log(ehPar(4))
+
+descontoInss = (salario) => {return salario - salario*0.1}
+
+console.log(descontoInss(5000))
